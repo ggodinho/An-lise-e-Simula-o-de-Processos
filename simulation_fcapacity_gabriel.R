26d585d4f74e516cc61e5d5d86cefeeda9e9151c
ggodinho <gabriel.godinho@gmail.com>
2018-11-10 03:05:04 -0200

    Melhorias e criação das funções para Monte Carlo e Moving Blocks Bootstrap

--- simulation_fcapacity_gabriel.R
+++ simulation_fcapacity_gabriel.R
@@ -60,37 +60,43 @@ plot(mstl_decomp)
 plotForecastErrors(mstl_decomp[,4])
 Box.test(mstl_decomp[,4], type = "Ljung-Box") #se p-valor <0.05, rejeito H0 (erros iid) com 5% de significância
 
+mstl_agg <- mstl_decomp[,2] + mstl_decomp[,3] #Série STL (sem remainder)
+mstl_res <- mstl_decomp[,4] #Remainder
 mean_mstl <- mean(mstl_decomp[,4]) #Média ~ 0
 sd_mstl <- sd(mstl_decomp[,4]) #Desvio padrão
-mstl_agg <- mstl_decomp[,2] + mstl_decomp[,3] #Série STL (sem remainder)
 
 #Simulando Monte Carlo
-n_series <- 100
-mc_mstl_series <- array(dim = c(n_series,744))
-monthly_mean_mstl_series <- array(dim = c(n_series,24))
-n_days = 1 #Quantos dias plotar
-
-plot(mstl_agg[1:(24*n_days)],ylim = c(0,1), type = "l", col = "red")
-for (i in 1:n_series){
-  set.seed(i^2);mc_mstl_series[i,] <- mstl_agg[1:(24*n_days)] + 
-    rnorm(n = 744, mean = 0, sd = sd_mstl)
-  mstl_series_matrix <- transpose(data.table(matrix(mc_mstl_series[i,],24,31)))
-  monthly_mean_mstl_series[i,] <- colMeans(mstl_series_matrix)
-  lines(mc_mstl_series[i,1:(24*n_days)],ylim = c(0,1), type = "l", 
-        col=alpha("gray",0.5))
+remainder_mc <- function(n_series,sinal,n_days,mean_mc,sd_mc)
+{
+  #n_series <- 100
+  # = 1 #Número de dias para considerar os cenários
+  mc_mstl_series <- array(dim = c(n_series,n_days*24))
+  monthly_mean_mstl_series <- array(dim = c(n_series,24))
+  
+  plot(mstl_agg[1:(24*n_days)],ylim = c(0,1), type = "l", col = "red")
+  for (i in 1:n_series){
+    set.seed(i^2);mc_mstl_series[i,] <- mstl_agg[1:(24*n_days)] + 
+      rnorm(n = (n_days*24), mean_mc, sd_mc)
+    mstl_series_matrix <- transpose(data.table(matrix(mc_mstl_series[i,],24,31)))
+    monthly_mean_mstl_series[i,] <- colMeans(mstl_series_matrix)
+    lines(mc_mstl_series[i,1:(24*n_days)],ylim = c(0,1), type = "l", 
+          col=alpha("gray",0.5))
+  }
+  
+  fan(mc_mstl_series[,1:(24*n_days)], ln=c(5, 25, 50, 75, 95), alpha=0,ln.col="red")
+  lines(colMeans(mc_mstl_series[,1:(24*n_days)]),ylim = c(0,1), type = "l", col = "black", lw = 2)
+  axis(side=1, at=c(0,1:(n_days*24)))
+  
+  monthly_expscen_mstl <- colMeans(monthly_mean_mstl_series) #Valor esperado dos cenários para um dia típico
+  
+  #Outras opções para fanplot
+  #plot(monthly_expscen_mstl, type = "l",ylim = c(0,1), col="blue")
+  #fan(mc_distr,fan.col = sequential_hcl,ln=c(5, 25, 50, 75, 95))
+  #fan(mc_distr,ln=c(5, 25, 50, 75, 95), style = "spaghetti",n.spag = 100)
+  #fan(mc_mstl_distr, ln=c(5, 25, 50, 75, 95), alpha=0,ln.col="red")
 }
 
-fan(mc_mstl_series[,1:(24*n_days)], ln=c(5, 25, 50, 75, 95), alpha=0,ln.col="red")
-lines(colMeans(mc_mstl_series[,1:(24*n_days)]),ylim = c(0,1), type = "l", col = "black", lw = 2)
-axis(side=1, at=c(0,1:24))
-
-monthly_expscen_mstl <- colMeans(monthly_mean_mstl_series) #Valor esperado dos cenários para um dia típico
-
-#Outras opções para fanplot
-#plot(monthly_expscen_mstl, type = "l",ylim = c(0,1), col="blue")
-#fan(mc_distr,fan.col = sequential_hcl,ln=c(5, 25, 50, 75, 95))
-#fan(mc_distr,ln=c(5, 25, 50, 75, 95), style = "spaghetti",n.spag = 100)
-#fan(mc_mstl_distr, ln=c(5, 25, 50, 75, 95), alpha=0,ln.col="red")
+remainder_mc(500,mstl_agg,1,0,sd_mstl)
 
 # Fazer bootstrap dos ruidos para MSTL
 
@@ -98,7 +104,54 @@ monthly_expscen_mstl <- colMeans(monthly_mean_mstl_series) #Valor esperado dos c
 # MSTL + Bootstrap #
 ######################################################
 
-mstl_boot <- array(dim = c(n_series,length(series)))
+MBB_function <- function(n_series,sinal,residuos,l)
+{
+  n = length(residuos)
+  #l = 48
+  b = n - l + 1
+  bloco = NULL
+  for (i in 1:b) {
+    bloco = cbind(bloco, residuos[i:(i + l - 1)])
+  }
+  blocos = as.matrix(bloco)
+  simulacoes_boot_MBB = amostras_boot_MBB = matrix(NA, ncol = n_series, nrow = n)
+  for (i in 1:n_series) {
+    set.seed(i);selecao = sample(1:dim(bloco)[2], ceiling(n/l), replace = T)
+    ruido.bs.prel = c(blocos[, selecao])
+    ruido.bs = ruido.bs.prel[(length(ruido.bs.prel) - length(residuos) + 1):length(ruido.bs.prel)]
+    simulacoes_boot_MBB[, i] = ruido.bs
+    amostras_boot_MBB[, i] = sinal + simulacoes_boot_MBB[, i]
+  }
+  
+  amostras_boot_MBB <- as.matrix(transpose(data.table(amostras_boot_MBB[1:24,])))
+  mstl_boot_mean <- colMeans(amostras_boot_MBB)
+  plot(mstl_boot_mean[1:24],type = "l",ylim = c(0,1))
+  for (i in 1:n_series){
+    lines(amostras_boot_MBB[i,1:24],col=alpha("gray",0.5))
+  }
+  fan(amostras_boot_MBB, ln=c(5, 25, 50, 75, 95), alpha=0,ln.col="red")
+  lines(mstl_boot_mean, lw=2)
+}
+
+MBB_function(500,mstl_agg,mstl_res,l = 48)
+
+
+
+# mstl_res_boot <- as.matrix(transpose(data.table(tsboot(mstl_res, 
+#                                                   n.sim=1000, l=48))))
+# mstl_res_boot <- mstl_res_boot[,1:24]
+# 
+# mstl_boot <- mstl_agg[1:24] + mstl_res_boot
+# mstl_boot_mean <- colMeans(mstl_boot)
+
+plot(mstl_boot_mean,type = "l",ylim = c(0,1))
+for (i in 1:n_series){
+  lines(mstl_boot[i,],col=alpha("gray",0.5))
+}
+fan(mstl_boot[,1:(24*n_days)], ln=c(5, 25, 50, 75, 95), alpha=0,ln.col="red")
+lines(mstl_boot_mean, lw=2)
+
+plot(mstl_agg[1:24],ylim=c(0,1))
 
 #Comparar médias e QQPLOT da distribuição verificada em 2018 e distribuição simulada 
 
